d2025a64b894423f0fdac9e5db67bf1659d6d26c
jckim2 <jckim229@gmail.com>
2022-06-30 23:59:59 +0900

    modify Topology Sort

--- Topology Sort.cpp
+++ Topology Sort.cpp
@@ -4,21 +4,26 @@
 #define MAX 10
 
 using namespace std;
+ 
+int n, inDegree[MAX]; 
 
-int n, inDegree[MAX]; //inDgree는 진입차수
-vector<int> a[MAX];
+vector<int> a[MAX]; 
 
 void topologySort() {
 	int result[MAX];
 	queue<int> q;
-	//진입 차수가 0인 노드를 큐에 삽입합니다.
+	
+	// 진입 차수가 0인 노드를 큐에 삽입합니다.
+
 	for (int i = 1; i <= n; i++) {
 		if (inDegree[i] == 0) {
 			q.push(i);
 		}
 	}
+	// 정렬이 완전히 수행되려면 정확히 n개의 노드를 방문합니다.
 
 	for (int i = 1; i <= n; i++) {
+		// n개를 방문하기 전에 큐가 비어버리면 사이클이 발생한 것입니다.
 		if (q.empty()) {
 			printf("사이클이 발생했습니다.");
 			return;
@@ -28,6 +33,7 @@ void topologySort() {
 		result[i] = x;
 		for (int i = 0; i < a[x].size(); i++) {
 			int y = a[x][i];
+			// 새롭게 진입차수가 0이 된 정점을 큐에 삽입합니다.
 			if (--inDegree[y] == 0) {
 				q.push(y);
 			}
@@ -40,7 +46,9 @@ void topologySort() {
 }
 
 int main() {
+
 	n = 7;
+
 	a[1].push_back(2);
 	inDegree[2]++;
 	a[1].push_back(5);
@@ -55,5 +63,8 @@ int main() {
 	inDegree[6]++;
 	a[6].push_back(7);
 	inDegree[7]++;
+
 	topologySort();
+
+
 }
