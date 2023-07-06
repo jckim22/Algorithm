f64a281cfb4c6afe106696b6a1c7bbf049cefedb
jckim22 <jckim229@gmail.com>
2023-07-06 21:34:14 +0900

    20230706

---
+++ 문자열 교환.py
@@ -0,0 +1,12 @@
+a=input()
+acnt=a.count('a')
+a+=a[0:acnt-1]
+bmin=a[0:acnt].count('b')
+
+for x in range(len(a)-acnt-1):
+    if a[x:x+acnt].count('b')<bmin:
+        bmin=a[x:x+acnt].count('b')
+print(bmin)
+        
+        
+    
---
+++ 물병.py
@@ -0,0 +1,41 @@
+# n,k=map(int,input().split())
+# check = 0b000000000
+# cnt=0
+
+
+# while N>K:
+#     if N%2==1:
+#         check |= (1<<cnt)
+#         N+=1
+#     N/=2
+#     print(cnt,check)
+#     cnt+=1
+# print(check)
+    
+import sys
+
+n, k = map(int, input().split())
+
+count = 0;
+
+while bin(n).count('1') > k:
+    n +=1
+    count +=1
+print(count)    
+
+# while(True):
+    
+
+
+
+# cnt=0
+# A=1
+
+# while(N>K):
+#     R=N%2
+#     cnt+=R*A
+#     A*=2
+#     N//=2
+#     N+=R
+
+# print(cnt)
